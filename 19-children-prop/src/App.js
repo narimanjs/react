eb519dd02ae8984fdebc2e350d5cba5a98121cd8
Nariman <mirmanov1@mail.ru>
2023-09-26 11:22:30 +0600

    done lesson 20 useEffect

--- 19-children-prop/src/App.js
+++ 19-children-prop/src/App.js
@@ -4,8 +4,29 @@ import Wrapper from './components/Wrapper';
 function App() {
   return (
     <div className='App'>
-      <Wrapper>
-        <h2>hello from wrapper</h2>
+      <Wrapper color='lightblue'>
+        <h2>inside hello from wrapper</h2>
+        <p>
+          Accusamus quaerat officiis incidunt veritatis voluptate commodi
+          placeat omnis nulla. Aspernatur, excepturi, nam, repellendus fugit
+          eaque voluptatibus praesentium quas unde minima numquam deserunt
+          expedita asperiores deleniti commodi adipisci nemo at. Rem at nulla
+          accusantium. Sit voluptates repellendus quaerat quas?
+        </p>
+        <button>click App.js</button>
+      </Wrapper>
+      <Wrapper color='lightgreen'>
+        <h2>another hello from wrapper</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+          suscipit exercitationem hic soluta facilis fugit nostrum? Natus sequi
+          rem a, exercitationem perspiciatis doloremque repellendus ullam facere
+          aperiam sit! Nam, iste!
+        </p>
+        <input
+          type='text'
+          placeholder='Enter value'
+        />
       </Wrapper>
     </div>
   );
